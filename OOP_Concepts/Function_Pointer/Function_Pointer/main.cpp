8d139afb45f4e92305318d90ff039c29b7364de8
sciencethebird <sciencethebird@gmail.com>
2018-11-04 18:02:38 +0800

    Update OOP_Concepts/Function_Pointer/Function_Pointer/main.cpp

--- OOP_Concepts/Function_Pointer/Function_Pointer/main.cpp
+++ OOP_Concepts/Function_Pointer/Function_Pointer/main.cpp
@@ -6,14 +6,51 @@
 //  Copyright © 2018年 Sciencethebird. All rights reserved.
 //
 
-#include <iostream>
+///including Function Pointers and Lambda
 
-void Hello(){
-    std::cout << "Hello" << std::endl;
+#include <iostream>
+#include <vector>
+
+// 2.Function Pointer
+void Hello(int a){
+    std::cout << a << "Hello" << std::endl;
+}
+void LMAO(int a){
+    std::cout << a << "LMAO" << std::endl;
+}
+
+/// 3.Function Pointer Vector
+void PrintValue(int value){
+    std::cout << "Value: " << value << std::endl;
+}
+void ForEach(const std::vector<int>& values, void(*func)(int)){
+    for(int value : values)
+        func(value);
+}
+
+/// Function that eats functions
+void Print(int a, void(*function)(int)){
+    std::cout << "Function" << std::endl;
+    function(a);
 }
 
 int main(){
-    typedef void(*f_type)();
+    
+    //Function Pointer Declaration
+    typedef void(*f_type)(int);
     f_type  function = Hello;
-    function();
+    function(7);
+    
+    //2.Function Pointer
+    Print(69, Hello);
+    Print(69, LMAO);
+    
+    //Lambda
+    Print(87, [](int a)
+    {std::cout << a<< std::endl;}
+          );
+    
+    //3.Function Pointer Vector
+    std::vector<int> values = {1,5,2,6,7,4,3};
+    ForEach(values, PrintValue);
 }
